4a956206910d3b23dbe00adf854ca21f1a5e88e6
Ruben Vazquez Almanza <rvazquez@reachcore.com>
2021-04-01 17:58:59 -0600

    EndPoint

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_gridify import FlaskGridify
 from flask_table import Table, Col
 import sqlite3
-from app import app as application
 
 # Get some objects
 # class Item(object):
