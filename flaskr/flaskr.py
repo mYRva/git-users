dd802b4a8e09ab5abdff1b5e1e5716f5a6be5ae0
Ruben Vazquez Almanza <rvazquez@reachcore.com>
2021-04-01 21:36:13 -0600

    new files

---
+++ flaskr/flaskr.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_gridify import FlaskGridify
+from flask_table import Table, Col
+import sqlite3
+
+# Get some objects
+# class Item(object):
+#     def __init__(self, name, description):
+#         self.name = name
+#         self.description = description
+
+
+# def create_app(test_config=None):
+    # create and configure the app
+    #conn = sqlite3.connect("test.db")
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'test.db'),
+)
+
+db = SQLAlchemy(app)
+ 
+
+#with app.test_request_context('/git'):
+#    resp = app.process_response(resp)
+
+
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+ 
+
+from flaskr import routes
